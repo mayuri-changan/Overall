6f7e91e7f9e7d1c9ef4de0d5a1031b7eef0841f1
mayuri-changan <mayuri2001changan@gmail.com>
2026-08-17 22:54:57 +0530

    Find sum

---
+++ Overall/src/com/demo/Sum.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		System.out.println("two numbers are here");
+		int a=20;
+		int b=20;
+		int sum=a+b;
+		System.out.println("Total sum is here");
+		System.out.println("="+sum);
+
+	}
+
+}
